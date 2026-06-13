6e31b412b4ec8da06295934787c4e27fdf28d4a5
Lavanya Singh <luckysinghyt@gmail.com>
2026-06-13 10:53:57 +0530

    files add

---
+++ core/summarizer.py
@@ -0,0 +1,59 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
+import os 
+
+def get_llm():
+    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.3)
+
+
+def split_transcript(transcript: str) -> list:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 3000,
+        chunk_overlap = 200
+    )
+
+    return splitter.split_text(transcript)
+
+def summarize(transcript):
+
+    prompt = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            "Create a professional meeting summary in bullet points."
+        ),
+        ("human","{text}")
+    ])
+
+    chain = prompt | get_llm() | StrOutputParser()
+
+    return chain.invoke({
+        "text": transcript[:12000]
+    })
+
+def generate_title(transcipt : str) -> str:
+    llm = get_llm()
+
+    
+
+    title_chain = (
+        RunnablePassthrough() | RunnableLambda(lambda x:{"text":x}) | 
+        ChatPromptTemplate.from_messages([
+             (
+                "system",
+                "Based on the meeting transcript, generate a short professional meeting title "
+                "(max 8 words). Only return the title, nothing else.",
+            ),
+            ("human", "{text}"),
+        ])
+        | llm
+        |StrOutputParser()
+    )
+
+    return title_chain.invoke(transcipt[:2000])
+
+
+
