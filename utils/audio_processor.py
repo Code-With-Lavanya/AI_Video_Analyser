3a12099f9d5f0878a4b7665bfb87d0fb79fe23ab
Lavanya Singh <luckysinghyt@gmail.com>
2026-06-13 22:05:19 +0530

    Add files via upload

---
+++ utils/audio_processor.py
@@ -0,0 +1,114 @@
+import yt_dlp
+import subprocess
+import math
+import os
+
+DOWNLOAD_DIR = 'downloades'
+os.makedirs(DOWNLOAD_DIR,exist_ok = True)
+
+def download_youtube_audio(url :str) ->str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+    return filename
+
+
+
+def convert_to_wav(input_path: str) -> str:
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-y",
+            "-i",
+            input_path,
+            "-ac", "1",
+            "-ar", "16000",
+            output_path,
+        ],
+        check=True,
+    )
+
+    return output_path
+
+
+def get_audio_duration(wav_path):
+
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-v", "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            wav_path,
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+
+    return float(result.stdout.strip())
+
+
+def chunk_audio(wav_path: str, chunk_minutes: int = 5):
+
+    duration = get_audio_duration(wav_path)
+
+    chunk_seconds = chunk_minutes * 60
+
+    total_chunks = math.ceil(duration / chunk_seconds)
+
+    chunks = []
+
+    for i in range(total_chunks):
+
+        chunk_path = f"{wav_path}_chunk_{i}.wav"
+
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                wav_path,
+                "-ss",
+                str(i * chunk_seconds),
+                "-t",
+                str(chunk_seconds),
+                chunk_path,
+            ],
+            check=True,
+        )
+
+        chunks.append(chunk_path)
+
+    return chunks
+
+def process_input(source: str) -> list:
+    if source.startswith("http://") or source.startswith("https://"):
+        print("Detected YouTube URL. Downloading audio...")
+        wav_path = download_youtube_audio(source)
+    else:
+        print("Detected local file. Converting to WAV...")
+        wav_path = convert_to_wav(source)
+
+    print("Chunking audio...")
+    chunks = chunk_audio(wav_path)
+    print(f"Audio ready — {len(chunks)} chunk(s) created.")
+    return chunks
+
